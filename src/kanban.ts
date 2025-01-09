bec6bcad9c8dd9e447b83a7d3807a244371d3ef1
*SJrGDXtfAq <ochiai@digimake.co.jp>
2025-01-09 23:02:56 +0900

    TaskListクラスの作成

--- src/kanban.ts
+++ src/kanban.ts
@@ -1,4 +1,4 @@
-import { bound } from "./decorator/bindThis.js"
+import { bound } from './decorator/bindThis.js'
 
 interface Task {
   title: string
@@ -12,18 +12,18 @@ class TaskForm {
 
   constructor() {
     // form要素を取得
-    this.element = document.querySelector("#task-form")! // 非 null アサーション
+    this.element = document.querySelector('#task-form')! // 非 null アサーション
 
     // input要素を取得
-    this.titleInputEl = document.querySelector("#form-title")!
-    this.descriptionInputEl = document.querySelector("#form-description")!
+    this.titleInputEl = document.querySelector('#form-title')!
+    this.descriptionInputEl = document.querySelector('#form-description')!
 
     this.bindEvents()
   }
 
   private clearInputs(): void {
-    this.titleInputEl.value = ""
-    this.descriptionInputEl.value = ""
+    this.titleInputEl.value = ''
+    this.descriptionInputEl.value = ''
   }
 
   @bound
@@ -43,17 +43,61 @@ class TaskForm {
   }
 
   private bindEvents() {
-    this.element.addEventListener("submit", this.submitHandler)
+    this.element.addEventListener('submit', this.submitHandler)
 
     this.clearInputs()
   }
 
-  private makeNewTask():Task {
+  private makeNewTask(): Task {
     return {
       title: this.titleInputEl.value,
-      description: this.descriptionInputEl.value
+      description: this.descriptionInputEl.value,
     }
   }
 }
 
 new TaskForm()
+
+// 配列とインデックスアクセス型を使用したユニオン型の作成
+const TASK_STATUS = ['todo', 'working', 'done'] as const
+type TaskStatus = (typeof TASK_STATUS)[number] // インデックスアクセス型
+
+class TaskList {
+  templagteEl: HTMLTemplateElement
+  element: HTMLDivElement
+  private taskStatus: TaskStatus
+
+  constructor(templateId: string, _taskStatus: TaskStatus) {
+    // ターゲットのtemplate要素を取得
+    this.templateEl = document.querySelector(templateId)!
+
+    // template要素のコンテンツ（子要素）を複製。trueを渡すことですべての階層でクローンする。
+    const clone = this.templagteEl.content.cloneNode(true) as DocumentFragment
+
+    // クローンした子要素から、1つ目を取得
+    this.element = clone.firstElementChild as HTMLDivElement
+
+    // taskStatusプロパティを初期化
+    this.taskStatus = _taskStatus
+
+    this.setup()
+  }
+
+  // クローンした要素に情報を追加
+  setup() {
+    // カラムに表示する、タスクの進捗状況を示すラベルを設定
+    this.element.querySelector('h2')!.textContent = `${this.taskStatus}`
+    // ul要素にid属性を設定
+    this.element.querySelector('ul')!.id = `${this.taskStatus}`
+  }
+
+  mount(selector: string) {
+    const targetEl = document.querySelector(selector)!
+    targetEl.insertAdjacentElement('beforeend', this.element)
+  }
+}
+
+TASK_STATUS.forEach((status) => {
+  const list = new TaskList('#task-list-template', status)
+  list.mount('#container')
+})
